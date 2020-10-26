19ac393e2db82cd9fd790972c63df9dd383f5b89
Iza Duszyńska <iza.duszynska@wp.pl>
2020-10-26 12:08:12 +0100

    pierwsza wersja

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Counter App</h1>
       </header>
+      <Counter initValue={0} />
+      <Counter initValue={108}/>
     </div>
   );
 }
 
 export default App;
+
+
+// import './App.css';
+// import Counter from './Counter';
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//        <h1 className="App-title">Counter App in ReactJS</h1>  
+//       </header>
+//       <Counter initValue={0} />
+//       <Counter initValue={108} />
+//     </div>
+//   );
+// }
+
+
